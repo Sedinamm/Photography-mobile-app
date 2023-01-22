908604e1531e3b6e0b9765661e3819912ce71a4b
Sedinam <sedinam436@gmail.com>
2023-01-22 16:26:30 -0300

    added Intro.js

--- Screens/IntroScreen.js
+++ Screens/IntroScreen.js
@@ -1,10 +1,10 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native'
 import React from 'react'
 
 const IntroScreen = () => {
   return (
     <View>
-      <Text>IntroScreen</Text>
+      <Text style={{color:"white"}}>IntroScreen</Text>
     </View>
   )
 }
