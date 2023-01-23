7fe567d4f5a31fe122ce8cdecc095550aa0f21c8
Sedinam <sedinam436@gmail.com>
2023-01-23 17:31:15 -0300

    added text

--- Screens/IntroScreen.js
+++ Screens/IntroScreen.js
@@ -5,8 +5,15 @@ const IntroScreen = () => {
   return (
     <SafeAreaView>
     <View>
-      {/* <Text style={{color:"white"}}>IntroScreen</Text> */}
+      {/* Image */}
       <Image style={{width:400, height:822}} source={require('../Images/2.png')} />
+
+      {/* Text */}
+      <Text style={{color:"white", fontSize:50, position:"absolute", top:500, left:25, fontWeight:"bold", shadowOpacity:1}}>Piclancer</Text>
+      <Text style={{color:"#00FFF0", position:"absolute", fontSize:70, left:235,top:480, shadowOpacity:1}}>.</Text>
+
+      {/* Short Description */}
+      <Text style={{position:"absolute", top:560, left:25, color:"white",  fontSize:25, fontWeight:"bold", shadowOpacity:1}}>Your Copyright free Images {'\n'} App </Text>
     </View>
     </SafeAreaView>
   )
