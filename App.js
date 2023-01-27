592ddcf26cd4874c3a965323219cf9680dda6612
Sedinam <sedinam436@gmail.com>
2023-01-27 10:38:42 -0300

    initial commit

--- App.js
+++ App.js
@@ -27,7 +27,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
--- Screens/Signup.js
+++ Screens/Signup.js
@@ -1,14 +1,41 @@
-import { View, Text, SafeAreaView, TouchableOpacity, Image } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  TextInput,
+  SafeAreaView,
+} from "react-native";
 
-const Signup = () => {
+export default function Signup() {
+
   return (
-    <SafeAreaView>
-    <View>
-      <Text style={{color:"white"}}>Signup</Text>
+      
+    <View style={{ flex: 10, backgroundColor: "black", }}>
+
+      {/* Signup text */}
+      <View style={{ flex: 2, backgroundColor: "green" }}>
+        <Text style={{paddingLeft:10, paddingTop:60}}>Herh chale, eno eaasy ohh</Text>
+      </View>
+
+      {/* Text input and Btn */}
+      <View style={{flex: 5,backgroundColor: "#7448",}}>
+      <Text style={{paddingLeft:10, }}>Herh chale, eno eaasy ohh</Text>
+      </View>
+
+      {/* Text and Icons */}
+      <View style={{flex: 3,backgroundColor: "#4518",}}>
+      <Text style={{paddingLeft:10,}}>Herh chale, eno eaasy ohh</Text>
+      </View>
+
+      {/* Already have an account? */}
+      <View style={{flex: 1,backgroundColor: "red",}}>
+      <Text style={{paddingLeft:10, paddingTop:30}}>Herh chale, eno eaasy ohh</Text>
+      </View>
     </View>
-    </SafeAreaView>
-  )
+  );
 }
 
-export default Signup
+
+
+
