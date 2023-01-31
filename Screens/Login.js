5e023e54eda5caf67744c29d7b2d59e38d446c18
Sedinam <sedinam436@gmail.com>
2023-01-31 16:06:36 -0300

    added email

--- Screens/Login.js
+++ Screens/Login.js
@@ -8,8 +8,26 @@ const Login = () => {
       <View style={{flex:2, backgroundColor:"green"}}>
         <Text style={{marginLeft:20, paddingTop:114,fontSize:25, fontWeight:"bold", color:"white"}}>Login</Text>
       </View>
+
       {/* TextInput */}
-      <View style={{flex:5, backgroundColor:"yellow"}}></View>
+      <View style={{flex:5, backgroundColor:"violet"}}>
+      <View style={{top:-10}}>
+        {/* Email */}
+      <View style={{top: 20, margin:20, }}>
+            <TextInput
+              keyboardType="email-address"
+              placeholder="Email"
+              style={{
+                backgroundColor: "white",
+                padding: 20,
+                borderRadius: 10,
+              }}
+            />
+          </View>
+
+          {/* Password */}
+          </View>
+      </View>
       <View style={{flex:3, backgroundColor:"brown"}}></View>
     </View>
   )
