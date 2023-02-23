5545f8b4f118b3ed947dc0ca438ebe789e239094
Sedinam <sedinam436@gmail.com>
2023-02-23 13:31:09 -0300

    imported SafeAreaView

--- Screens/Profile.js
+++ Screens/Profile.js
@@ -1,4 +1,4 @@
-import { View, Text, Image, TouchableOpacity } from 'react-native'
+import { View, Text, Image, TouchableOpacity,SafeAreaView } from 'react-native'
 import React from 'react'
 
 const Profile = () => {
