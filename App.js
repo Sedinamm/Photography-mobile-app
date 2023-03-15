bd91b9a54906d7e11efc037b9a3031bd21c962b3
Sedinam <sedinam436@gmail.com>
2023-03-15 07:41:02 -0300

    installed react native vector icons

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import IntroScreen from './Screens/IntroScreen';
 import Signup from './Screens/Signup';
 import Login from './Screens/Login';
@@ -14,11 +16,11 @@ export default function App() {
   return (
     <View style={styles.container}>
       {/* <StatusBar style="auto" /> */}
-      {/* <IntroScreen/> */}
+      <IntroScreen/>
       {/* <Signup/> */}
       {/* <Login/> */}
       {/* <PrivacyPolicy/> */}
-      <Home/>
+      {/* <Home/> */}
       {/* <Profile/> */}
       {/* <Main/> */}
 
@@ -26,9 +28,51 @@ export default function App() {
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#000',
   },
 });
+
+
+
+
+
+
+
+
+
+
+
+// In App.js in a new project
+
+// import * as React from 'react';
+// import { View, Text } from 'react-native';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// function HomeScreen() {
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//       <Text>Home Screen</Text>
+//     </View>
+//   );
+// }
+
+// const Stack = createNativeStackNavigator();
+
+// function App() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen name="Home" component={HomeScreen} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
+
+
+// export default App;
