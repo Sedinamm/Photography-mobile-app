b72ea1152a0039dd3ccdba9bfa445859dd44ce59
Sedinam <sedinam436@gmail.com>
2023-04-25 17:01:55 -0200

    initial commit

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,9 +1,11 @@
 import { View, Text, TouchableOpacity, Image,SafeAreaView, TextInput } from 'react-native'
 import React from 'react'
+import TabNavigator from '../Screens/TabNavigator';
+
 
 const Home = () => {
   return (
-    <View style={{flex:10, backgroundColor:"yellow"}}>
+    <View style={{flex:10, backgroundColor:"blue"}}>
       <View>
       <Text>Home</Text>
       </View>
