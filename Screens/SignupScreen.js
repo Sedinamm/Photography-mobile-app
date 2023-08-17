7e2ce7638b6d0f0f877fd55da17add084e3c1238
Sedinam <sedinam436@gmail.com>
2023-08-17 15:22:34 -0300

    added images to the images folder

--- Screens/SignupScreen.js
+++ Screens/SignupScreen.js
@@ -1,110 +1,86 @@
+import React from "react";
 import {
   View,
   Text,
   Image,
-  TouchableOpacity,
   TextInput,
-  SafeAreaView,
+  TouchableOpacity,
+  StyleSheet,
 } from "react-native";
 
-export default function SignupScreen() {
+const SignUpScreen = () => {
   return (
-    <View style={{ flex: 10, backgroundColor: "black" }}>
-      {/* Signup text */}
-      <View style={{ flex: 2, backgroundColor: "black" }}>
-        <Text
-          style={{
-            marginLeft: 20,
-            paddingTop: 114,
-            fontSize: 25,
-            fontWeight: "bold",
-            color: "white",
-          }}
-        >
-          Sign Up Today
-        </Text>
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        {/* Replace these image sources with your actual images */}
+        <Image source={require("../Images/3.jpg")} style={styles.image} />
+        <Image source={require("../Images/4.jpg")} style={styles.image} />
+        <Image source={require("../Images/5.jpg")} style={styles.image} />
       </View>
-
-      {/* Text input and Btn */}
-      <View style={{ flex: 5, backgroundColor: "#000" }}>
-        {/* Email */}
-        <View style={{ top: -10 }}>
-          <View style={{ top: 20, margin: 20 }}>
-            <TextInput
-              keyboardType="email-address"
-              placeholder="Email"
-              style={{
-                backgroundColor: "white",
-                padding: 20,
-                borderRadius: 10,
-              }}
-            />
-          </View>
-          {/* Password */}
-          <View style={{ top: 5, margin: 20 }}>
-            <TextInput
-              placeholder="Password"
-              style={{
-                backgroundColor: "white",
-                padding: 20,
-                borderRadius: 10,
-              }}
-            />
-          </View>
-          {/* Phone Number */}
-          <View style={{ top: -9, margin: 20 }}>
-            <TextInput
-              placeholder="Phone Number"
-              style={{
-                backgroundColor: "white",
-                padding: 20,
-                borderRadius: 10,
-              }}
-            />
-          </View>
-          {/* Text{Already have an account?} */}
-          <View>
-            <Text style={{ color: "white", marginLeft: 143, bottom: 20 }}>
-              Already have an account?
-              <TouchableOpacity>
-                <Text style={{ color: "#00FFF0", bottom: -4 }}>Sign In</Text>
-              </TouchableOpacity>
-            </Text>
-          </View>
-          {/* Sign up Button */}
-          <View>
-            <TouchableOpacity>
-              <View
-                style={{
-                  height: 55,
-                  width: 250,
-                  backgroundColor: "#00FFF0",
-                  justifyContent: "center",
-                  borderRadius: 10,
-                  alignItems: "center",
-                  alignSelf: "center",
-                }}
-              >
-                <Text style={{ color: "#000" }}>Sign in</Text>
-              </View>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </View>
-
-      {/* Text and Icons */}
-      <View style={{ flex: 3, backgroundColor: "#000" }}>
-        <Text style={{ paddingLeft: 10, color: "white" }}>
-          Google, Phone Number
-        </Text>
-      </View>
-
-      {/* Already have an account? */}
-      <View style={{ flex: 1, backgroundColor: "#000" }}>
-        <Text style={{ paddingLeft: 10, paddingTop: 30, color: "white" }}>
-          By signing up, you agee to our terms of use
-        </Text>
-      </View>
+      <Text style={styles.bottomText}>
+        Signup today and get access to over 12,000+ copyright-free images
+      </Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        placeholderTextColor="#999"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        placeholderTextColor="#999"
+        secureTextEntry
+      />
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Sign Up</Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "black",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+  imageContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 20,
+  },
+  image: {
+    width: 110,
+    height: 180,
+    borderRadius: 10,
+  },
+  bottomText: {
+    textAlign: "center",
+    color: "#fff",
+    marginBottom: 20,
+  },
+  input: {
+    width: "100%",
+    height: 40,
+    backgroundColor: "#333",
+    color: "#fff",
+    marginBottom: 10,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+  },
+  button: {
+    backgroundColor: "#007BFF",
+    paddingVertical: 12,
+    borderRadius: 5,
+    width: "100%",
+  },
+  buttonText: {
+    color: "#fff",
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+});
+
+export default SignUpScreen;
