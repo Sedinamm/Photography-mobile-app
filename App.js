ef690127d6b816292d61ec78c1b323ec02d5d6ba
Sedinam <sedinam436@gmail.com>
2023-08-18 17:38:09 -0300

    initial commit

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Entypo } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
 
 import IntroScreen from "./Screens/IntroScreen";
 import Signup from "./Screens/SignupScreen";
@@ -14,12 +16,85 @@ import NotificationScreen from "./Screens/NotificationScreen";
 import UploadScreen from "./Screens/UploadScreen";
 // import Main from "./Screens/Main";
 
+// const Tab = createBottomTabNavigator();
+
+// const HomeTab = () => {
+//   return (
+//     // Home Bottom Tab
+//     <Tab.Navigator
+//       screenOptions={{
+//         headerShown: false,
+//         tabBarStyle: { backgroundColor: "#000" },
+//         tabBarActiveTintColor: "white",
+//       }}
+//     >
+//       <Tab.Screen
+//         name="Home"
+//         component={HomeScreen}
+//         options={{
+//           tabBarIcon: ({ color, size }) => (
+//             <Entypo name="home" size={size} color={color} />
+//           ),
+//         }}
+//       />
+
+//       {/* Service Screen Bottom Tab */}
+//       <Tab.Screen
+//         name="NotificationScreen"
+//         component={NotificationScreen}
+//         options={{
+//           tabBarIcon: ({ color, size }) => (
+//             <Ionicons name="people" size={size} color={color} />
+//           ),
+//         }}
+//       />
+
+//       {/* Add Screen Bottom Tab */}
+//       <Tab.Screen
+//         name="UploadScreen"
+//         component={UploadScreen}
+//         options={{
+//           tabBarIcon: ({ color, size }) => (
+//             <Ionicons name="add-circle-outline" size={size} color={color} />
+//           ),
+//         }}
+//       />
+//     </Tab.Navigator>
+//   );
+// };
+
+// const MyNestedStack = () => {
+//   return (
+//     <NavigationContainer independent={true}>
+//       <Stack.Navigator>
+//         <Stack.Screen
+//           name="HomeTab"
+//           component={HomeTab}
+//           options={{ headerShown: false }}
+//         />
+//         <Stack.Screen
+//           name="Home"
+//           component={HomeScreen}
+//           options={{ headerShadowVisible: false }}
+//         />
+
+//         <Stack.Screen name="UploadScreen" component={UploadScreen} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// };
+
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
+        {/* <Stack.Screen
+          name="MyNestedStack"
+          component={MyNestedStack}
+          options={{ headerShown: false }}
+        /> */}
         <Stack.Screen name="IntroScreen" component={IntroScreen} />
         <Stack.Screen name="LoginScreen" component={LoginScreen} />
         <Stack.Screen name="SignupScreen" component={Signup} />
@@ -36,10 +111,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#000',
-//   },
-// });
