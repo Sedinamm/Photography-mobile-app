b5ff2d8785e9b1821d8ec8a7f1ac26759a1ef1c4
Sedinam <sedinam436@gmail.com>
2023-08-28 10:54:21 -0300

    initial commit

--- Screens/ProfileScreen.js
+++ Screens/ProfileScreen.js
@@ -1,14 +1,65 @@
+// import {
+//   View,
+//   Text,
+//   Image,
+//   TouchableOpacity,
+//   SafeAreaView,
+// } from "react-native";
+// import React from "react";
+
+// const ProfileScreen = () => {
+//   return (
+//     <View style={{ flex: 10, alignItems: "center", justifyContent: "center" }}>
+//       <Text>Profile Screen</Text>
+//     </View>
+//   );
+// };
+
+// export default ProfileScreen;
+
 import {
   View,
   Text,
-  Image,
   TouchableOpacity,
+  Image,
   SafeAreaView,
+  TextInput,
 } from "react-native";
 import React from "react";
+import { auth } from "../firebase";
+import { useNavigation } from "@react-navigation/native";
 
 const ProfileScreen = () => {
+  const navigation = useNavigation();
+
+  const handleSignOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        navigation.replace("IntroScreen");
+      })
+      .catch((error) => alert(error.message));
+  };
+
   return (
+    // <View style={{ flex: 10, alignItems: "center", justifyContent: "center" }}>
+    //   <Text>Email: {auth.currentUser?.email}</Text>
+
+    //   <TouchableOpacity
+    //     onPress={handleSignOut}
+    //     style={{
+    //       backgroundColor: "blue",
+    //       width: "60%",
+    //       padding: 15,
+    //       borderRadius: 10,
+    //       alignItems: "center",
+    //       marginTop: 40,
+    //     }}
+    //   >
+    //     <Text style={{ color: "white" }}>Sign Out</Text>
+    //   </TouchableOpacity>
+    // </View>
+
     <View style={{ flex: 10, alignItems: "center", justifyContent: "center" }}>
       <Text>Profile Screen</Text>
     </View>
